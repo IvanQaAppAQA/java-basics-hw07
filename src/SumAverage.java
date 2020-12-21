0bf23ebe65d0f60c0cc9f97f7d37245dd1f60a5d
Ivan Golub <ivangolub1993@gmail.com>
2020-12-21 17:38:43 +0200

    Homework average

--- src/SumAverage.java
+++ src/SumAverage.java
@@ -3,6 +3,7 @@
  */
 public class SumAverage {
 
+
   /**
    * Computes sum of running integers from a lower bound to an upper bound.
    *
@@ -11,9 +12,15 @@ public class SumAverage {
    * @return sum of integers
    */
   public static int sum(int lowerBound, int upperBound) {
+    int sum = 0;
+    for (int i = lowerBound; i <= upperBound; i++) {
+      sum +=i;
+    }
+    return sum;
     // TODO fill in code here using for loop and replace the return statement
-    return -1;
-  }
+    }
+
+
 
   /**
    * Computes sum of running integers from a lower bound to an upper bound within an array.
@@ -22,8 +29,12 @@ public class SumAverage {
    * @return sum of integers
    */
   public static int sum(int[] numbers) {
-    // TODO fill in code here using for each loop and replace the return statement
-    return -1;
+    int sum = 0;
+    for (int h : numbers) {
+      sum += h;
+    }
+    return sum;
+    // TODO fill in code here using FOR EACH loop and replace the return statement
   }
 
   /**
@@ -34,8 +45,17 @@ public class SumAverage {
    * @return average of integers
    */
   public static double average(int lowerBound, int upperBound) {
+    double count = 0;
+    double sum = 0;
+    int i = lowerBound;
+    while (i <= upperBound) {
+      sum +=i;
+      count++;
+      i++;
+    }
+    double average = sum/count;
+    return average;
     // TODO fill in code here using while loop and replace the return statement
-    return -1;
   }
 
   /**
@@ -45,7 +65,16 @@ public class SumAverage {
    * @return average of integers
    */
   public static double average(int[] numbers) {
+    double sum = 0;
+    int i = 0;
+    do {
+      sum += numbers[i];
+      i++;
+    } while (i < numbers.length);
+
+    double average = sum / numbers.length;
+    return average;
     // TODO fill in code here using do-while loop and replace the return statement
-    return -1;
+
   }
 }
